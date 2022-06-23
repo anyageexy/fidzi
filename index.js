f2ce6821ebc7bf78c0abb7f385e74bf710c1c89c
anyageexy <anya.alexeeva2001@mail.ru>
2022-06-24 01:45:12 +0500

    Ok

--- index.js
+++ index.js
@@ -1,39 +1,89 @@
-"use strict"
+// "use strict"
 
-const lazyArr = document.querySelectorAll('img[data-src], source[data-srcset]');
-const windowHeight = document.documentElement.clientHeight;
+// const lazyArr = document.querySelectorAll('img[data-src], source[data-srcset]');
+// const windowHeight = document.documentElement.clientHeight;
 
-let lazyImagesPos = [];
-if(lazyArr.length > 0){
-  lazyArr.forEach(img => {
-    if(img.dataset.src || img.dataset.srcset){
-      lazyImagesPos.push(img.getBoundingClientRect().top + scrollY);
-      lazyScrollCheck();
+// let lazyImagesPos = [];
+// if(lazyArr.length > 0){
+//   lazyArr.forEach(img => {
+//     if(img.dataset.src || img.dataset.srcset){
+//       lazyImagesPos.push(img.getBoundingClientRect().top + scrollY);
+//       lazyScrollCheck();
+//     }
+//   });
+// }
+
+// window.addEventListener("scroll", lazyScroll);
+
+// function lazyScroll(){
+//   if(document.querySelectorAll('img[data-src], source[data-srcset]').length > 0){
+//     lazyScrollCheck();
+//   }
+// }
+
+
+// function lazyScrollCheck(){
+//   let imgIndex = lazyImagesPos.findIndex(
+//     item => scrollY > item - windowHeight
+//   );
+//   if(imgIndex>=0){
+//     if(lazyArr[imgIndex].dataset.src){
+//       lazyArr[imgIndex].src = lazyArr[imgIndex].dataset.src;
+//       lazyArr[imgIndex].removeAttribute('data-src');
+//     } else if (lazyArr[imgIndex].dataset.srcset){
+//       lazyArr[imgIndex].srcset = lazyArr[imgIndex].dataset.srcset;
+//       lazyArr[imgIndex].removeAttribute('data-srcset');
+//     }
+//     delete lazyImagesPos[imgIndex];
+//   }
+// }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages;    
+
+  if ("IntersectionObserver" in window) {
+    lazyloadImages = document.querySelectorAll(".lazy");
+    var imageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          var image = entry.target;
+          image.classList.remove("lazy");
+          imageObserver.unobserve(image);
+          console.log('работает')
+        }
+      });
+    });
+
+    lazyloadImages.forEach(function(image) {
+      imageObserver.observe(image);
+    });
+  } else {  
+    var lazyloadThrottleTimeout;
+    lazyloadImages = document.querySelectorAll(".lazy");
+    
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+
+      lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+      }, 20);
     }
-  });
-}
 
-window.addEventListener("scroll", lazyScroll);
-
-function lazyScroll(){
-  if(document.querySelectorAll('img[data-src], source[data-srcset]').length > 0){
-    lazyScrollCheck();
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
   }
-}
-
-
-function lazyScrollCheck(){
-  let imgIndex = lazyImagesPos.findIndex(
-    item => scrollY > item - windowHeight
-  );
-  if(imgIndex>=0){
-    if(lazyArr[imgIndex].dataset.src){
-      lazyArr[imgIndex].src = lazyArr[imgIndex].dataset.src;
-      lazyArr[imgIndex].removeAttribute('data-src');
-    } else if (lazyArr[imgIndex].dataset.srcset){
-      lazyArr[imgIndex].srcset = lazyArr[imgIndex].dataset.srcset;
-      lazyArr[imgIndex].removeAttribute('data-srcset');
-    }
-    delete lazyImagesPos[imgIndex];
-  }
-}
+})
